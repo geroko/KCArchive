0cb0ca6f53e95fb08d93884ad8229fa302b383be
geroko <nw94@tutanota.com>
2020-11-02 15:05:48 -0500

    Added file and banned search options

--- src/forms.py
+++ src/forms.py
@@ -9,6 +9,7 @@ from wtforms.fields.html5 import DateField
 
 from src import app
 from src.models import Post, File, Report
+from src.utils import get_ip_address
 
 def prevent_blank_search(form, submit):
 	if not form.post_num.data and not form.subject.data and not form.message.data:
@@ -19,9 +20,12 @@ class SearchForm(FlaskForm):
 	subject = StringField(validators=[Optional()])
 	message = StringField(validators=[Optional()])
 	is_op = BooleanField()
+	banned = BooleanField()
 	flag = SelectField(choices=[('Show All', 'Show All')] + [f for f in app.config['FLAG_MAP'].items()])
 	start_date = DateField(validators=[Optional()])
 	end_date = DateField(validators=[Optional()])
+	filename = StringField(validators=[Optional()])
+	orig_name = StringField(validators=[Optional()])
 	submit = SubmitField()
 
 	# Get data from url args if formdata is empty, disable csrf check
@@ -33,7 +37,7 @@ class SearchForm(FlaskForm):
 		super(SearchForm, self).__init__(*args, **kwargs)
 
 	def get_results(self):
-		posts = Post.query.order_by(Post.date.desc())
+		posts = Post.query.group_by(Post.post_num).order_by(Post.date.desc())
 		if self.post_num.data:
 			posts = posts.filter(Post.post_num == self.post_num.data)
 		if self.subject.data:
@@ -44,10 +48,16 @@ class SearchForm(FlaskForm):
 			posts = posts.filter(Post.flag == self.flag.data)
 		if self.is_op.data == True:
 			posts = posts.filter(Post.is_op == True)
+		if self.banned.data == True:
+			posts = posts.filter(Post.ban_message != None)
 		if self.start_date.data:
 			posts = posts.filter(Post.date > self.start_date.data)
 		if self.end_date.data:
 			posts = posts.filter(Post.date < self.end_date.data)
+		if self.filename.data:
+			posts = posts.join(Post.files_contained).filter(File.filename.contains(self.filename.data))
+		if self.orig_name.data:
+			posts = posts.join(Post.files_contained).filter(File.orig_name.contains(self.orig_name.data))
 		return posts
 
 def report_cooldown(form, submit):
